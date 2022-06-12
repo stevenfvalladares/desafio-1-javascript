d6e65cebaa1d8119d8a020865b7049a7bdd29404
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-06-11 20:25:38 -0400

    Script for calculation of total completed

--- assets/js/calculate_total.js
+++ assets/js/calculate_total.js
@@ -1,2 +1,32 @@
 // connection testing
 console.log('linked file')
+
+// variables
+const price = 400000
+var quantity, calculate, total  
+
+// get HTML elements
+quantity = document.getElementById('quantity')
+calculate = document.getElementById('btn')
+
+// function
+function calculateTotal() {
+
+    total = price * parseInt(quantity.value)
+
+    if (isNaN(parseInt(quantity.value))) {
+        alert('Es necesario introducir un número válido')
+    } else {
+        document.getElementById('total').innerHTML = ' '.concat(Intl.NumberFormat('de-DE').format(total))
+        document.getElementById('selected_quantity').innerHTML = ' '.concat(quantity.value)
+    }
+
+}
+
+
+
+
+
+
+
+

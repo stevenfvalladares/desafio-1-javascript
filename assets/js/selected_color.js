4330af95c201175129d5ce0ab168853c3fc47f46
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-06-12 16:59:43 -0400

    Script to select product color completed

--- assets/js/selected_color.js
+++ assets/js/selected_color.js
@@ -1,4 +1,21 @@
 // connection testing
 console.log('linked file')
 
+// variables
+var selector, color
 
+// get HTML elements
+selector = document.querySelector('.form-select')
+color = document.querySelector('.color-circle')
+
+// funtion
+function selectedColor() {
+
+    if (selector.value === 'Color' ) {
+        alert('Es necesario seleccionar el color')
+        color.style.backgroundColor = 'whitesmoke'
+    } else {
+        color.style.backgroundColor = selector.value    
+    }
+
+}
